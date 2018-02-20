d4bb8a862d182748dba46a7a108fe7d5fabc6aa3
Jorge Saud <jorgelsaud@gmail.com>
2018-02-20 18:04:08 -0300

    fix webhook

--- src/shortcodes/tasaRemesasShortcode.php
+++ src/shortcodes/tasaRemesasShortcode.php
@@ -25,19 +25,19 @@ class tasaRemesasShortcode extends Singleton{
 	}
 	protected function prepareView(){
         $tasa=number_format($this->tasa/100,2,',','.');
-        $html="<h2 style='text-align: center;'>Tasa $tasa</h2>";
+        $html="<h2 style='text-align: center;'>Tasa $tasa $/Bs</h2>";
         $tasa=number_format($this->tasa/100*20000,2,',','.');
-        $html.="<p style='text-align: center;'>20.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>20.000$=$tasa Bs.</p>";
         $tasa=number_format($this->tasa/100*40000,2,',','.');
-        $html.="<p style='text-align: center;'>40.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>40.000$=$tasa Bs.</p>";
         $tasa=number_format($this->tasa/100*50000,2,',','.');
-        $html.="<p style='text-align: center;'>50.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>50.000$=$tasa Bs.</p>";
         $tasa=number_format($this->tasa/100*100000,2,',','.');
-        $html.="<p style='text-align: center;'>100.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>100.000$=$tasa Bs.</p>";
         $tasa=number_format($this->tasa/100*500000,2,',','.');
-        $html.="<p style='text-align: center;'>500.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>500.000$=$tasa Bs.</p>";
         $tasa=number_format($this->tasa/100*1000000,2,',','.');
-        $html.="<p style='text-align: center;'>1.000.000$=$tasa</p>";
+        $html.="<p style='text-align: center;'>1.000.000$=$tasa Bs.</p>";
 
         $this->view=$html;
 	}
